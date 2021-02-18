bc1b86b5ebcb3677e1f8d80be4aaa53462c69b73
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2021-02-18 12:06:55 +0200

    Add files via upload
    Paragraph Tags

---
+++ L1.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 11:48:05 2021
+
+@author: Alaisha Naidu
+Name: HTML L1 
+Creds; Jen Simmons and CodePen
+"""
+
+# Paragraphs 
+
+<p> To indicate a single paragraph, an opening p tag is used at the beginning of the 
+text and then at the end of the paragraph, a closing p tag is used to close the paragraph like so </p>
