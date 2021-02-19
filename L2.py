d91e25ef0485c99c83e79f3cf02864c7483fe9f7
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2021-02-19 15:26:47 +0200

    Headline tags

---
+++ L2.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 11:48:05 2021
+
+@author: Alaisha Naidu
+Name: HTML L2
+Creds; Jen Simmons and CodePen
+"""
+
+# Paragraphs 
+
+<p> To indicate a single paragraph, an opening p tag is used at the beginning of the 
+text and then at the end of the paragraph, a closing p tag is used to close the paragraph like so </p>
+
+#Headlines 
+
+<H1> This is the largest and most pronounced headline </H1>
+<H2> This is the second largest. Could be for subheadings </H2>
+<H3> Headlines also require opening and closing tags </H3> 
+<H4> There are six headline sizes </H4>
+<H5> This is the second smallest headline font size </H3> 
+<H6> This is the smallest headline font size </H6> 
