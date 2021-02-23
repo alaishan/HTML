aa61f11352b768ab35c789dce4f6b0ff011fb91b
Alaisha Naidu <69754098+alaishan@users.noreply.github.com>
2021-02-23 12:48:32 +0200

    Lists

---
+++ L4.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 18 11:48:05 2021
+
+@author: Alaisha Naidu
+Name: HTML L2
+Creds; Jen Simmons and CodePen
+"""
+
+# Paragraphs 
+
+<p> To indicate a single paragraph, an opening p tag is used at the beginning of the 
+text and then at the end of the paragraph, a closing p tag is used to close the paragraph like so </p>
+
+#Headlines 
+
+<H1> This is the largest and most pronounced headline </H1>
+<H2> This is the second largest. Could be for subheadings </H2>
+<H3> Headlines also require opening and closing tags </H3> 
+<H4> There are six headline sizes </H4>
+<H5> This is the second smallest headline font size </H3> 
+<H6> This is the smallest headline font size </H6> 
+
+# Bold and Italics 
+
+  #italics
+<em> tag is used to wrap something that should be verbally emphasized </em>
+<i> tag is used to wrap the name of something, or something with no special meaning </i>
+
+  #bold
+<strong> element conveys importance and/or seriousness, conveys meaning </strong>
+<b> element is a way of styling or marking something, no alternative mood </b> 
+
+# Lists 
+
+  #Undordered List 
+<ul>
+    <li> inidcates list item </li>
+    <li> inidcated by bullets </li>
+    <li> tab/space indentation is for human comprehension only </li>
+</ul> 
+
+  #Odered List
+<ol>
+    <li> inidcates list item </li>
+    <li> inidcated by numbers </li>
+    <li> tab/space indentation is for human comprehension only </li>    
+</ol>
+
+  #Definition List
+<dl>
+    <dt> Definition Term 1 </dt>
+        <dd> definition description of term 1 </dd> 
+        <dd> can have more than one defition for each term </dd> 
+    <dt> Definition Term 2 </dt> 
+        <dd> definition description of term 2 </dd>
+    <dt> Definition Term 3 </dt> 
+        <dd> definition description of term 3 </dd>
+</dl>        
+    
+    
+    
+    
